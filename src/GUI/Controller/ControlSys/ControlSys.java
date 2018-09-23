1d64e110f6039ad68a119e3afb139054bb253288
Eirik Gribbestad Gustafsson <eirikgg94@gmail.com>
2018-09-23 16:55:30 +0200

    Added video playback

--- src/GUI/Controller/ControlSys/ControlSys.java
+++ src/GUI/Controller/ControlSys/ControlSys.java
@@ -1,9 +1,38 @@
 package GUI.Controller.ControlSys;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+import java.io.File;
+
 
 public class ControlSys {
+    // Media player.
+    @FXML private MediaView mediaView;
+
+    public ControlSys() {
+    }
+
+    @FXML private void handleStartBtnAction() {
+        playVideo();
+    }
+
+    private void playVideo() {
+
+        // Loads Video from file.
+        Media media = new Media(getClass().getResource("../../Video/dummyVideo.mp4").toExternalForm());
+
+        // Adds Video to .
+        MediaPlayer player = new MediaPlayer(media);
+
+        // Adds the Video.
+        this.mediaView.setMediaPlayer(player);
+
+        // Plays the Video.
+        player.play();
+    }
+
+
 }
