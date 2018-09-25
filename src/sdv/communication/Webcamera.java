c7e1843d2b24c75f12d0481857f759300980ad04
Eirik Gribbestad Gustafsson <eirikgg94@gmail.com>
2018-09-25 10:45:54 +0200

    Added webcam picture capture

--- src/sdv/communication/Webcamera.java
+++ src/sdv/communication/Webcamera.java
@@ -1,17 +1,34 @@
 package sdv.communication;
 
-/**
- * Reads stream a UDP web-camera stream.
- *
- * @author: Eirik G. Gustafsson
- * @version: 23.09.2018.
- */
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
+
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
+import com.github.sarxos.webcam.WebcamResolution;
+
 public class Webcamera {
+    // Webcamera.
+    private Webcam webcam;
 
 
-    public Webcamera() {
-
+    public Webcamera(Webcam webcam) {
+        this.webcam = Webcam.getDefault();
     }
 
+    public void preStart() throws Exception {
+        webcam.setViewSize(WebcamResolution.VGA.getSize());
+        webcam.open();
+    }
 
+    public void postStop() throws Exception {
+        webcam.close();
+    }
+
+    public BufferedImage getImage() {
+        return webcam.getImage();
+    }
 }
--- src/sdv/gui/controller/ControlSys.java
+++ src/sdv/gui/controller/ControlSys.java
@@ -1,37 +1,33 @@
 package sdv.gui.controller;
 
+import com.github.sarxos.webcam.Webcam;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 
-public class ControlSys {
-    // Media player.
-    @FXML private MediaView mediaView;
+public class ControlSys{
+    // Computers webcamera.
+    @FXML private ImageView imageView;
+    // Image from web-cam.
+    private BufferedImage grabbedImage;
+    // Stop camera flag.
+    private boolean stopCamera = false;
 
-    public ControlSys() {
-    }
+    public ControlSys() {}
 
     /**
      * Handles what happens when the start button.
      */
-    @FXML private void handleStartBtnAction() {
-        playVideo();
-    }
-
-    private void playVideo() {
-
-        // Loads video from file.
-        Media media = new Media(getClass().getResource("../video/dummyVideo.mp4").toExternalForm());
-
-        // Adds video to media player.
-        MediaPlayer player = new MediaPlayer(media);
-
-        // Adds the video.
-        this.mediaView.setMediaPlayer(player);
-
-        // Plays the video.
-        player.play();
+    @FXML private void handleStartBtnAction() throws IOException {
+        Webcam webcam = Webcam.getWebcams().get(1);
+        webcam.open();
+        BufferedImage bufferedImage = webcam.getImage();
+        Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+        this.imageView.setImage(image);
     }
 }
